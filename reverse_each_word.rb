db4bf38d02f21f47760fac5db10467237f65aa93
Laura Radville <laura.radville@gmail.com>
2019-06-27 18:20:18 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,3 +3,10 @@ def reverse_each_word(string)
   array.each{ |n| puts n.reverse! }
   string = array.join(" ")
 end
+
+def reverse_each_word(string)
+  array = string.split(" ")
+  array.collect{ |n| puts n.reverse! }
+  string = array.join(" ")
+  string
+end
